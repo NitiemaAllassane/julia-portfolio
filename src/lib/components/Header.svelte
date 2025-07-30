39257a52a85c58e86cc349335adf5a56d19c5161
Nitiema Allassane <allassanenitiema@gmail.com>
2025-07-30 14:55:46 +0000

    Home page structure

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+    import portfolioLogo from '../../../static/images/logo.svg';
+    import { AlignJustify } from 'lucide-svelte';
+
+</script>
+
+<header class=" bg-primary flex justify-between items-center py-10 px-6 rounded-[20px] ">
+        <a href="/">
+            <img src={portfolioLogo} alt="Portfolio logo">
+        </a>
+
+        <nav class="hidden md:block">
+            <ul class="flex items-center gap-12">
+                <li><a href="/" class=" font-light">PROJECTS</a></li>
+                <li><a href="/" class=" font-light">ABOUT</a></li>
+                <li><a href="/" class=" font-light">CONTACT</a></li>
+            </ul>
+        </nav>
+
+        <button class="text-2xl font-semi-bold md:hidden">
+            <AlignJustify />
+        </button>
+</header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,92 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Icon from '@iconify/svelte'
+    import { MoveUpRight } from "lucide-svelte";
+</script>
+
+<main>
+    <!-- section presentation -->
+    <section>
+        <header>
+            <figure>
+                <img src="/images/flower.svg" alt="" aria-hidden="true">
+            </figure>
+        </header>
+        <h1>
+            Artist Redefining <span>Architecture</span> with AI-Driven Design
+        </h1>
+    </section>
+    <!-- Julia photo -->
+    <figure>
+        <img src="/images/julia.svg" alt="Julia avatar">
+    </figure>
+
+    <section>
+        <a href="/">
+            <header>
+                <div>
+                    <p>Musea</p>
+                    <span>
+                        <MoveUpRight />
+                    </span>
+                </div>
+                <!-- image -->
+                 <figure>
+                    <img src="/images/image.svg" alt="" aria-hidden="true">
+                 </figure>
+            </header>
+            <ul>
+                <li>Elara</li>
+                <li>Verve</li>
+                <li>Zephyr</li>
+            </ul>
+        </a>
+    </section>
+
+    <section>
+        <header>
+            <figure>
+                <img src="/images/circleIcon.svg" alt="" aria-hidden="true">
+            </figure>
+        </header>
+        <p>
+            Julia Huang is an innovative AI artist, renowned for blending 
+            cutting-edge technology with creative expression. 
+            Based in LA, she crafts unique digital art experiences accessible globally.
+        </p>
+    </section>
+
+    <!-- contact -->
+    <section>
+        <header>
+            <p>Have some questions ?</p>
+            <span>
+                <MoveUpRight />
+            </span>
+        </header>
+        <h2>Contact me</h2>
+    </section>
+
+    <article>
+        <ul>
+            <li>
+                <a href="/">
+                    <Icon icon="simple-icons:instagram" />
+                    <!-- <span>Instagram</span> -->
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    <Icon icon="simple-icons:x" />
+                    <!-- <span>X</span> -->
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    <Icon icon="simple-icons:linkedin" />
+                    <!-- <span>LinkedIn</span> -->
+                </a>
+            </li>
+        </ul>
+    </article>
+
+</main>
